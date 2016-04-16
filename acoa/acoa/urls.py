4e3ec6c189572f70ac6eec700e9c076c941e3fa4
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-04-16 14:34:07 -0500

    We've established base-line.

--- acoa/acoa/urls.py
+++ acoa/acoa/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog import views as blogviews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', blogviews.index),
+    url(r'^(?P<slug>[\w\-]+)/$', blogviews.post)
 ]
